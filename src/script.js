4842e83e997b2c4b71ccdc5feaee81ea3f77ade8
Anton Zamay <tony.zamay@mail.ru>
2024-02-04 19:34:56 +0100

    switching models

--- src/script.js
+++ src/script.js
@@ -61,47 +61,49 @@ const firstModelColor1 = "red";
 const firstModelColor2 = "yellow";
 const secondModelColor1 = "blue";
 const secondModelColor2 = "white";
+const modelArray = [];
 
 
 //Load Models
 // 1)
-// gltfLoader.load("/models/1/1.glb", (glb) => {
-//   //Increasing the number of vertices
-//   const samplerMesh = new MeshSurfaceSampler(glb.scene.children[0]).build();
-//   const particlesNumber = 5000;
-//   const particlesGeometry = new THREE.BufferGeometry();
-//   const particlesArray = new Float32Array(particlesNumber * 3);
-//   for (let i = 0; i < particlesNumber; i++) {
-//     const position = new THREE.Vector3();
-//     samplerMesh.sample(position);
-//     particlesArray.set(
-//       [position.x, position.y, position.z],
-//       i * 3
-//     );
-//   }
-//   particlesGeometry.setAttribute("position", new THREE.BufferAttribute(particlesArray, 3));
-//
-//   //Changing model into particles
-//   glb.scene.children[0] = new THREE.Points(
-//     particlesGeometry,
-//     new THREE.RawShaderMaterial({
-//       vertexShader: vShader,
-//       fragmentShader: fShader,
-//       uniforms: {
-//         u_color_1: { value: new THREE.Color(firstModelColor1) },
-//         u_color_2: { value: new THREE.Color(firstModelColor2) },
-//       },
-//       depthTest: false,
-//       blending: THREE.AdditiveBlending,
-//     })
-//   );
-//
-//
-//   glb.scene.children[0].scale.set(0.7, 0.7, 0.7);
-//   glb.scene.children[0].position.x = 0.5;
-//   glb.scene.children[0].rotation.y = Math.PI * 0.5;
-//   scene.add(glb.scene);
-// });
+gltfLoader.load("/models/1/1.glb", (glb) => {
+  //Increasing the number of vertices
+  const samplerMesh = new MeshSurfaceSampler(glb.scene.children[0]).build();
+  const particlesNumber = 5000;
+  const particlesGeometry = new THREE.BufferGeometry();
+  const particlesArray = new Float32Array(particlesNumber * 3);
+  for (let i = 0; i < particlesNumber; i++) {
+    const position = new THREE.Vector3();
+    samplerMesh.sample(position);
+    particlesArray.set(
+      [position.x, position.y, position.z],
+      i * 3
+    );
+  }
+  particlesGeometry.setAttribute("position", new THREE.BufferAttribute(particlesArray, 3));
+
+  //Changing model into particles
+  glb.scene.children[0] = new THREE.Points(
+    particlesGeometry,
+    new THREE.RawShaderMaterial({
+      vertexShader: vShader,
+      fragmentShader: fShader,
+      uniforms: {
+        u_color_1: { value: new THREE.Color(firstModelColor1) },
+        u_color_2: { value: new THREE.Color(firstModelColor2) },
+      },
+      depthTest: false,
+      blending: THREE.AdditiveBlending,
+    })
+  );
+
+
+  glb.scene.children[0].scale.set(0.7, 0.7, 0.7);
+  glb.scene.children[0].position.x = 0.5;
+  glb.scene.children[0].rotation.y = Math.PI * 0.5;
+  scene.add(glb.scene);
+  modelArray[0] = glb.scene;
+});
 
 // 2)
 gltfLoader.load("/models/1/2.glb", (glb) => {
@@ -141,12 +143,23 @@ gltfLoader.load("/models/1/2.glb", (glb) => {
   glb.scene.children[0].rotation.x = -Math.PI * 0.5;
   glb.scene.children[0].position.y = -0.2;
   glb.scene.children[0].rotation.z = -Math.PI * 0.5;
-  scene.add(glb.scene);
+  modelArray[1] = glb.scene;
 });
 
 //OrbitControl
 const orbitControls = new OrbitControls(camera, canvas);
 orbitControls.enableDamping = true;
+
+// Button click listeners
+buttons[0].addEventListener("click", () => {
+  scene.add(modelArray[0]);
+  scene.remove(modelArray[1]);
+})
+
+buttons[1].addEventListener("click", () => {
+  scene.add(modelArray[1]);
+  scene.remove(modelArray[0]);
+})
 
 //Clock Class
 const clock = new THREE.Clock();
