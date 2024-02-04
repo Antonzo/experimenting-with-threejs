5b691b77eb8f56ea188668ca119be4a8b5b7d644
Anton Zamay <tony.zamay@mail.ru>
2024-02-04 15:07:47 +0100

    added 2 models

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { MeshSurfaceSampler } from "three/examples/jsm/math/MeshSurfaceSampler";
 import vShader from "./shaders/vertex.glsl";
 import fShader from "./shaders/fragment.glsl";
 
@@ -53,8 +54,64 @@ const canvas = document.querySelector(".draw");
 const renderer = new THREE.WebGLRenderer({
   canvas,
 });
-renderer.setClearColor("#27282c", 1.0);
+renderer.setClearColor("#444444", 1.0);
 renderer.setSize(aspect.width, aspect.height);
+
+const firstModelColor1 = "red";
+const firstModelColor2 = "yellow";
+const secondModelColor1 = "blue";
+const secondModelColor2 = "white";
+
+
+//Load Models
+// 1)
+gltfLoader.load("/models/1/1.glb", (glb) => {
+  //Changing model into particles
+  glb.scene.children[0] = new THREE.Points(
+      glb.scene.children[0].geometry,
+      new THREE.RawShaderMaterial({
+          vertexShader: vShader,
+          fragmentShader: fShader,
+          uniforms: {
+              u_color_1: { value: new THREE.Color(firstModelColor1) },
+              u_color_2: { value: new THREE.Color(firstModelColor2) },
+          },
+          depthTest: false,
+          blending: THREE.AdditiveBlending,
+      })
+  );
+
+
+  glb.scene.children[0].scale.set(0.7, 0.7, 0.7);
+  glb.scene.children[0].position.x = 0.5;
+  glb.scene.children[0].rotation.y = Math.PI * 0.5;
+  scene.add(glb.scene);
+});
+
+// 2)
+gltfLoader.load("/models/1/2.glb", (glb) => {
+  //Changing model into particles
+  glb.scene.children[0] = new THREE.Points(
+      glb.scene.children[0].geometry,
+      new THREE.RawShaderMaterial({
+        vertexShader: vShader,
+        fragmentShader: fShader,
+        uniforms: {
+          u_color_1: { value: new THREE.Color(secondModelColor1) },
+          u_color_2: { value: new THREE.Color(secondModelColor2) },
+        },
+        depthTest: false,
+        blending: THREE.AdditiveBlending,
+      })
+  );
+
+
+  glb.scene.children[0].scale.set(0.3, 0.3, 0.3);
+  glb.scene.children[0].rotation.x = -Math.PI * 0.5;
+  glb.scene.children[0].position.y = -0.2;
+  glb.scene.children[0].rotation.z = -Math.PI * 0.5;
+  scene.add(glb.scene);
+});
 
 //OrbitControl
 const orbitControls = new OrbitControls(camera, canvas);
